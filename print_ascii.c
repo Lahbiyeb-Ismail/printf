b860c01029748d4482ff3731e5564d77c2da3714
Kamal Abdelmalek <malikleet1337@gmail.com>
2023-11-14 03:18:44 -0800

    updating print_ascii.c

--- print_ascii.c
+++ print_ascii.c
@@ -42,3 +42,59 @@ void print_ascii(int *char_len, va_list args, flags_t *flags)
 
 	va_end(args);
 }
+/**
+ * print_hex_digit - Prints a hexadecimal digit in the format
+ * '0'-'9' or 'A'-'F'.
+ *
+ *  * @char_len: the total number of characters written
+ *  * @digit: string to print
+ *
+ * Description: This function takes an integer representing a
+ * hexadecimal digit and prints it
+ * to the standard output in the specified format.
+ * If the digit is in the range
+ * 0-9, it is printed as '0' to '9'. If the digit is 10 or greater,
+ * it is printed
+ * as 'A' to 'F'.
+ *
+ * Return: The number of characters printed.
+ *
+ */
+
+void print_hex_digit(int digit, int *char_len)
+{
+	if (digit > 9)
+		(*char_len) += _putchar(digit + 55);
+	else
+		(*char_len) += _putchar(digit + '0');
+}
+
+/**
+ * nonprintable_char - Prints a non-printable character in hexadecimal format.
+ *
+ *  * @ascii: The non-printable ASCII character to be printed.
+ *  * @char_len: A pointer to the variable tracking the number
+ * of characters printed.
+ *
+ * Description: This function takes a non-printable ASCII character and
+ * prints it in the
+ * format '\x' followed by the ASCII code value in hexadecimal (always 2
+ * characters).
+ *
+ * Return: void
+ *
+ */
+
+void nonprintable_char(char ascii, int *char_len)
+{
+	int divide, module;
+
+	(*char_len) += _putchar('\\');
+	(*char_len) += _putchar('x');
+	divide = ascii / 16;
+	module = ascii % 16;
+
+
+	print_hex_digit(divide, char_len);
+	print_hex_digit(module, char_len);
+}
